e705a30fc8a79a03f189754e0a2822a2128f45a5
Bel-kun <bellaire.vivien22@gmail.com>
2022-06-21 09:49:56 -0400

    Homepage > Add mvp

---
+++ sources/javascript/main.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('.container')
+const startButton = container.querySelector('.start-button')
+const scene = container.querySelector('.scene')
+
+const start = () =>
+{
+    startButton.style.display = 'none'
+    scene.style.display = 'inherit'
+    console.log('start')
+
+    for(let i = 0; i < 1; i++)  
+    {
+        const target = document.createElement('div')
+        setTimeout(() => 
+        {
+            target.classList.add('target')
+            target.style.top = Math.random() * 100 + '%'
+            target.style.left = Math.random() * 100 + '%'
+            scene.append(target)
+            const timeStart = Date.now()
+
+            let isSpaceDown = false
+            window.addEventListener('keydown', function(press)
+            {
+                if(press.code === 'Space' && isSpaceDown === false)
+                {
+                    isSpaceDown = true
+                    press.preventDefault()                             
+                    target.remove(target)
+
+                    if(scene.children.length === 0)
+                    {              
+                        startButton.style.display = 'block'
+                        console.log('fini')
+                        const timeEnd = Date.now()
+                        const timeElapsed = timeEnd - timeStart
+                        const timeElapsedFormated = timeElapsed / 1000 + 's'
+                        console.log(timeElapsed)
+                        startButton.innerHTML = 'RESTART<br>' + timeElapsedFormated
+                        
+                    } 
+                }
+            })
+            window.addEventListener('keyup', function(press)
+            {
+                if(press.code === 'Space')
+                {
+                    // isSpaceDown = false
+                }
+            })  
+        }, Math.random() * 1200);
+          
+        
+        console.log(scene)   
+    }                 
+}
+startButton.addEventListener('click',start)  
